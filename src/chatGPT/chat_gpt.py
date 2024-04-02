560ba338b3059b9655bd3cde7f54f56e34323545
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-04-02 20:17:06 -0300

    agregamos comentarios

--- src/chatGPT/chat_gpt.py
+++ src/chatGPT/chat_gpt.py
@@ -15,29 +15,34 @@ buffer_messages = [role_system]
 def respuesta_chat_gpt():
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-0125",
-            messages=buffer_messages,
-            temperature=1,
-            max_tokens=50
+            model="gpt-3.5-turbo-0125",  # version que se usara
+            messages=buffer_messages,  # historial de mensajes de la conversación al modelo de lenguaje
+            temperature=1,   # controla la aleatoriedad en la generación de respuestas
+            max_tokens=50,   # establece el máximo de tokens que puede tener la respuesta
+            frequency_penalty=0,   # ajusta la probabilidad de que se repitan palabras o frases en la respuesta
+            presence_penalty=0   # ajusta la probabilidad de que se incluyan palabras raras
         )
 
-        return response.choices[0].message.content
+        return response.choices[0].message.content  #retorna el contenido del mensaje ingresado por el usuario
     except ValueError as error:
         print(error)
 
 conversacion = False
 
+# verifica si se ingreso algun argumento y si ese es "--convers" para iniciar la conversacion con chatgpt
 if (len(sys.argv) > 1 and sys.argv[1] == "--convers"):
     conversacion = True
     print("MODO CONVERSACION")
 
 print("ingrese una consulta no vacia o ingrese 'salir' si quiere abandonar")
 
+# ciclo que permite al usuario interactuar con chatgpt
 while True:
     try:
         consulta = input("You: ")
 
         if consulta:
+            # guarda la ultima consulta ingresada por el usuario
             ultima_consulta = consulta
 
             if (consulta == "salir"):
@@ -46,11 +51,12 @@ while True:
                 consulta = ultima_consulta
 
             if conversacion:  # cuando esta en modo conversacion
-                buffer_messages.append({"role": "user", "content": consulta})
+                buffer_messages.append({"role": "user", "content": consulta})  #guarda la consulta en el buffer con las anteriores
 
                 try:
+                    # obtiene la respuesta de chatgpt y la muestra
                     respuesta = respuesta_chat_gpt()
-                    buffer_messages.append({"role": "assistant", "content": respuesta})                                        
+                    buffer_messages.append({"role": "assistant", "content": respuesta})   #guarda la respuesta de chatgpt en el buffer con las anteriores usando el role assistent                                   
                     print("chatGPT:", respuesta)
                 except ValueError as error:
                     print(error)
@@ -58,6 +64,7 @@ while True:
                 buffer_messages = [role_system, {"role": "user", "content": consulta}]
                     
                 try:
+                    # obtiene la respuesta de chatgpt y la muestra
                     respuesta = respuesta_chat_gpt()
                     print("chatGPT:", respuesta)
                 except ValueError as error:
