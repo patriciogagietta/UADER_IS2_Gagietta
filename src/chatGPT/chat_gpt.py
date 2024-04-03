1932fff0107222ad7eb5e35a3a643fa1f4321714
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-04-03 16:06:03 -0300

    correcciones sugeridas por pylint

--- src/chatGPT/chat_gpt.py
+++ src/chatGPT/chat_gpt.py
@@ -1,7 +1,12 @@
+"""
+Este modulo proporciona una implementacion de chat usando el modelo gpt-3.5-turbo-0125 de openAI,
+permite generar respuestas a partir de consultas.
+"""
+
+import sys
 import os
 import openai
 from dotenv import load_dotenv
-import sys
 
 # cargar las variables de entorno desde el archivo .env
 load_dotenv()
@@ -13,25 +18,29 @@ role_system = {"role": "system","content": ""}
 buffer_messages = [role_system]
 
 def respuesta_chat_gpt():
+    """Esta funcion envía una solicitud utilizando la api key de openIA
+    para obtener una respuesta a partir de un historial de mensajes de conversación o de una consulta"""
+
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-0125",  # version que se usara
-            messages=buffer_messages,  # historial de mensajes de la conversación al modelo de lenguaje
+            messages=buffer_messages,  # historial de mensajes
             temperature=1,   # controla la aleatoriedad en la generación de respuestas
             max_tokens=50,   # establece el máximo de tokens que puede tener la respuesta
             frequency_penalty=0,   # ajusta la probabilidad de que se repitan palabras o frases en la respuesta
             presence_penalty=0   # ajusta la probabilidad de que se incluyan palabras raras
         )
 
-        return response.choices[0].message.content  #retorna el contenido del mensaje ingresado por el usuario
-    except ValueError as error:
-        print(error)
+        return response.choices[0].message.content #retorna el contenido del mensaje ingresado por el usuario
+    except ValueError as value_error:
+        print(value_error)
+        return None
 
-conversacion = False
+CONVERSACION = False
 
 # verifica si se ingreso algun argumento y si ese es "--convers" para iniciar la conversacion con chatgpt
 if (len(sys.argv) > 1 and sys.argv[1] == "--convers"):
-    conversacion = True
+    CONVERSACION = True
     print("MODO CONVERSACION")
 
 print("ingrese una consulta no vacia o ingrese 'salir' si quiere abandonar")
@@ -45,31 +54,30 @@ while True:
             # guarda la ultima consulta ingresada por el usuario
             ultima_consulta = consulta
 
-            if (consulta == "salir"):
+            if consulta == "salir":
                 break
-            elif (consulta == "\033[A"):         # "\033[A" mover el cursor una linea para arriba con la flecha para arriba
+
+            if consulta == "\033[A":         # "\033[A" mover el cursor una linea para arriba con la flecha para arriba
                 consulta = ultima_consulta
 
-            if conversacion:  # cuando esta en modo conversacion
-                buffer_messages.append({"role": "user", "content": consulta})  #guarda la consulta en el buffer con las anteriores
-
+            if CONVERSACION:  # cuando esta en modo conversacion
+                buffer_messages.append({"role": "user", "content": consulta})#guarda la consulta en el buffer con las anteriores
                 try:
                     # obtiene la respuesta de chatgpt y la muestra
                     respuesta = respuesta_chat_gpt()
-                    buffer_messages.append({"role": "assistant", "content": respuesta})   #guarda la respuesta de chatgpt en el buffer con las anteriores usando el role assistent                                   
+                    buffer_messages.append({"role": "assistant", "content": respuesta})#guarda la respuesta de chatgpt en el buffer con las anteriores usando el role assistent
                     print("chatGPT:", respuesta)
-                except ValueError as error:
-                    print(error)
+                except ValueError as value_error:
+                    print(value_error)
             else:  # cuando NO esta en modo conversacion
                 buffer_messages = [role_system, {"role": "user", "content": consulta}]
-                    
                 try:
                     # obtiene la respuesta de chatgpt y la muestra
                     respuesta = respuesta_chat_gpt()
                     print("chatGPT:", respuesta)
-                except ValueError as error:
-                    print(error)
+                except ValueError as value_error:
+                    print(value_error)
         else:
             print("ingresa una consulta válida")
-    except ValueError as error:
-        print(error)
+    except ValueError as value_error:
+        print(value_error)
