933dde2efabe008d748368e1d50baf46a095f9a4
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-06-04 16:42:40 -0300

    implementacion del patron de creacion Singleton

--- src/RRR_TP1/getJasonModificado.py
+++ src/RRR_TP1/getJasonModificado.py
@@ -1,13 +1,44 @@
 import json, sys
-jsonfile = sys.argv[1]
 
-if len(sys.argv) > 2:
-    jsonkey = sys.argv[2]   
-else:
-    jsonkey = "token1"
-    
-with open(jsonfile, 'r') as myfile:
-    data = myfile.read()
-obj = json.loads(data)
+class SingletonMeta(type):
 
-print (str(obj[jsonkey]))
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class RecuperacionToken(metaclass=SingletonMeta):
+
+    def recuperacion_token(self, jsonfile, jsonkey):
+
+        with open(jsonfile, 'r') as myfile:
+            data = myfile.read()
+        obj = json.loads(data)
+
+        print(str(obj[jsonkey]))
+
+
+if __name__ == "__main__":
+
+    s1 = RecuperacionToken()
+    s2 = RecuperacionToken()
+
+    jsonfile = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        jsonkey = sys.argv[2]   
+    else:
+        jsonkey = "token1"    
+
+
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+        print()
+
+        s1.recuperacion_token(jsonfile,jsonkey)
+    else:
+        print("Singleton failed, variables contain different instances.")
