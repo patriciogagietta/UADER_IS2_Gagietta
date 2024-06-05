b49fcb25e5b024ad12e4de94c7b0b7aba9ed5c43
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-06-05 12:11:31 -0300

    implementacion del patron Iterator para manejar el historial de pagos

--- src/RRR_TP1/get_jason_modificado.py
+++ src/RRR_TP1/get_jason_modificado.py
@@ -4,6 +4,10 @@ Copyright UADERFCyT-IS2©2024 todos los derechos reservados
 
 
 """
+from __future__ import annotations
+from collections.abc import Iterable, Iterator
+from typing import Any, List, Dict
+
 import json
 import sys
 
@@ -39,11 +43,14 @@ class RecuperacionToken(metaclass=SingletonMeta):
             "token2": 2000
         }
         self.jsonfile = jsonfile
+        self.historial_pagos = pagosCollection()
 
-    def realizar_pago(self, monto):
+    def realizar_pago(self, monto, numero_pedido):
         for token in self.saldo:
             if self.saldo[token] >= monto:
                 self.saldo[token] -= monto
+                pago = {"Numero de pedido": numero_pedido, "Token": token, "Monto": monto}
+                self.historial_pagos.add_pago(pago)
                 print(f"Pago {monto} desde el token '{token}'")
                 return
             
@@ -60,6 +67,70 @@ class RecuperacionToken(metaclass=SingletonMeta):
         # Se produce cuando se intenta acceder a un archivo que no existe
         except FileNotFoundError:
             print("El archivo JSON no existe o lo ingreso de manera incorrecta")
+
+    def listado_pagos(self):
+        return iter(self.historial_pagos)
+
+
+
+class AlphabeticalOrderIterator(Iterator):
+    """
+    Los iteradores concretos implementan varios algoritmos transversales. estas clases
+    memorizar en todo momento la posición de recorrido actual.
+    """
+
+    """
+    El atributo `_position` almacena la posición transversal actual. Un iterador puede
+    Tiene muchos otros campos para almacenar el estado de iteración, especialmente cuando
+    se supone que funciona con un tipo particular de colección.
+    """
+
+    _position: int = None
+
+    """
+    Este atributo indica la dirección transversal.
+    """
+    _reverse: bool = False
+
+    def __init__(self, collection: pagosCollection, reverse: bool = False) -> None:
+        self._collection = collection
+        self._reverse = reverse
+        self._position = -1 if reverse else 0
+
+    def __next__(self):
+        """
+        El método __next__() debe devolver el siguiente elemento de la secuencia. En
+        llegando al final, y en llamadas posteriores, debe lanzar StopIteration.
+        """
+        try:
+            value = self._collection[self._position]
+            self._position += -1 if self._reverse else 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
+
+
+class pagosCollection(Iterable):
+    """
+    Las colecciones concretas proporcionan uno o varios métodos para recuperar material fresco.
+    instancias de iterador, compatibles con la clase de colección.
+    """
+    def __init__(self, collection: List[Any] = []) -> None:
+        self._collection = collection
+
+    def __iter__(self) -> AlphabeticalOrderIterator:
+        """
+        El método __iter__() devuelve el objeto iterador en sí; de forma predeterminada,
+        devuelve el iterador en orden ascendente.
+        """
+        return AlphabeticalOrderIterator(self._collection)
+
+    def get_reverse_iterator(self) -> AlphabeticalOrderIterator:
+        return AlphabeticalOrderIterator(self._collection, True)
+
+    def add_pago(self, item: Any):
+        self._collection.append(item)
 
 
 if __name__ == "__main__":
@@ -81,14 +152,18 @@ if __name__ == "__main__":
 
 
     if id(s1) == id(s2):
-        s1.realizar_pago(500)
-        s1.realizar_pago(500)
-        s1.realizar_pago(500)
-        s1.realizar_pago(500)
-        s1.realizar_pago(500)
-        s1.realizar_pago(500)
-        s1.realizar_pago(500)
-        s1.realizar_pago(500)
-        s1.realizar_pago(500)
+
+        s1.realizar_pago(500, 1)
+        s1.realizar_pago(500, 2)
+        s1.realizar_pago(500, 3)
+        s1.realizar_pago(500, 4)
+        s1.realizar_pago(500, 5)
+        s1.realizar_pago(500, 6)
+        s1.realizar_pago(500, 7)
+        
+        print()
+        print("Historial de pagos:")
+        for pago in s1.listado_pagos():
+            print(pago)
 
 
